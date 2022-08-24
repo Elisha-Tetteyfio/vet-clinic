bb0a751c95c28162328e2c7219e03c0223e0622b
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-08-24 14:08:39 +0000

    Update animals table

--- queries.sql
+++ queries.sql
@@ -8,3 +8,13 @@ SELECT name, escape_attempts from animals WHERE (weight_kg > 10.5);
 SELECT * from animals WHERE (neutered);
 SELECT * from animals WHERE (name != 'Gabumon');
 SELECT * from animals WHERE (weight_kg >= 10.4) AND (weight_kg <=17.3);
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT SUM(weight_kg)/COUNT(weight_kg) AS average_weight FROM animals;
+SELECT neutered, COUNT(escape_attempts) AS escape_attempts FROM animals WHERE escape_attempts>0 GROUP BY neutered;
+SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+SELECT 
+    species, SUM(escape_attempts)/COUNT(escape_attempts) AS average_escape_attempt FROM animals 
+  WHERE(date_of_birth>'1990-01-01' AND date_of_birth<'2000-12-31')
+  GROUP BY species;
